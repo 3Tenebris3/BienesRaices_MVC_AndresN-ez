2ed4719194dcfd8cc40a6079eb2f1c2f6794ad06
Andrés Núñez <63027901+3Tenebris3@users.noreply.github.com>
2024-01-06 17:12:33 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,13 +4,13 @@ dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASSWORD ?? '', {
     host:process.env.BD_HOST,
-    port: '3307',
+    port: '3306',
     dialect: 'mysql',
     define: {
         timestamps: true
     },
     pool: {
-        max: 5,
+        max: 10,
         min: 0,
         acquire: 30000,
         idle: 10000
